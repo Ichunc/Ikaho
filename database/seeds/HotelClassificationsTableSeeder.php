5042fc7ad91aef76cebc44894735336e6ed9797f
mmrakt <48762864+mmrakt@users.noreply.github.com>
2020-05-18 19:11:00 +0900

    hotelsテーブルとhotel_classificationsテーブルのシーダーファイル作成

---
+++ database/seeds/HotelClassificationsTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class HotelClassificationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            'name' => 'シティホテル'
+        ];
+        DB::table('hotel_classifications')->insert($data);
+
+        $data = [
+            'name' => 'リゾートホテル'
+        ];
+        DB::table('hotel_classifications')->insert($data);
+        
+        $data = [
+            'name' => 'ビジネスホテル'
+        ];
+        DB::table('hotel_classifications')->insert($data);
+        
+        $data = [
+            'name' => '旅館'
+        ];
+        DB::table('hotel_classifications')->insert($data);
+        
+        $data = [
+            'name' => '民宿'
+        ];
+        DB::table('hotel_classifications')->insert($data);
+        
+        $data = [
+            'name' => 'ペンション'
+        ];
+        DB::table('hotel_classifications')->insert($data);
+    }
+}
