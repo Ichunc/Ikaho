3ee588f817f5a1acf45f61682b9ce62e6fabf46c
mmrakt <48762864+mmrakt@users.noreply.github.com>
2020-05-20 11:30:34 +0900

    宿情報の登録確認画面の修正
    ・各項目の入力確認
    ・チェックイン・アウト時間の項目追加

--- resources/views/admin/hotel_add_confirm.blade.php
+++ resources/views/admin/hotel_add_confirm.blade.php
@@ -1,48 +1,55 @@
 @extends('layout.admin')
 
-@section('title', '宿の管理')
+@section('title', '宿情報の塘路国確認')
 
 @section('content')
 <div class="search-form">
     <h2>宿情報の登録確認</h2>
-    <form action="" method='post' enctype='multipart/form-data'>
+    <form action="{{ route('hotel.create') }}" method='post' enctype='multipart/form-data'>
         @csrf
-
         <table>
-            <input type="hidden" name='hotel_name' value=''>
+            <input type="hidden" name='hotel_name' value="{{ $data['hotel_name']}}">
             <tr>
-
                 <th>宿名：</th>
-                <td>伊香保の宿</td>
+                <td>{{ $data['hotel_name']}}</td>
             </tr>
-            <input type="hidden" name='hotel_code' value=''>
+            <input type="hidden" name='hotel_code' value="{{ $data['hotel_code']}}">
             <tr>
-
                 <th>宿分類：</th>
-                <td>3.旅館</td>
+                <td>{{ $data['hotel_code']}}</td>
             </tr>
-            <input type="hidden" name='hotel_postal' value=''>
+            <input type="hidden" name='hotel_postal' value="{{ $data['hotel_postal']}}">
             <tr>
                 <th>郵便番号：</th>
-                <td>123-4567</td>
+                <td>{{ $data['hotel_postal']}}</td>
             </tr>
-            <input type="hidden" name='hotel_address' value=''>
+            <input type="hidden" name='hotel_address' value="{{ $data['prefectures'].$data['city'].$data['block']}}">
             <tr>
                 <th>住所：</th>
-                <td>群馬県渋川市伊香保町</td>
+                <td>{{ $data['prefectures'].$data['city'].$data['block']}}</td>
             </tr>
-            <input type="hidden" name='hotel_tel' value=''>
+            <input type="hidden" name='hotel_tel' value="{{ $data['hotel_tel']}}">
             <tr>
                 <th>電話番号：</th>
-                <td>03-0000-0000</td>
+                <td>{{ $data['hotel_tel']}}</td>
             </tr>
-            <input type="hidden" name='hotel_image' value=''>
+            <input type="hidden" name='checkin_time' value="{{ $data['checkin_time']}}">
+            <tr>
+                <th>チェックイン時間：</th>
+                <td>{{ $data['checkin_time']}}</td>
+            </tr>
+            <input type="hidden" name='checkout_time' value="{{ $data['checkout_time']}}">
+            <tr>
+                <th>チェックアウト時間：</th>
+                <td>{{ $data['checkout_time']}}</td>
+            </tr>
+            <input type="hidden" name='hotel_image' value="">
             <tr>
                 <th>宿のイメージ写真：</th>
                 <td></td>
             </tr>
         </table>
-        <button><a href="">戻る</a></button>
+        <button><a href="{{ route('hotel.add') }}">戻る</a></button>
         <button type='submit'>登録</button>
     </form>
 
