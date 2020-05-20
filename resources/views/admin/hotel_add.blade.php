832cd434940ea5641c4c6a00a3fd1868887d2a34
mmrakt <48762864+mmrakt@users.noreply.github.com>
2020-05-20 11:26:20 +0900

    宿登録画面修正
    ・チェックイン・チェックアウト時間の項目を追加
    ・「戻る」ボタンのリンク追加
    ・各入力項目のvalueにold関数追加

--- resources/views/admin/hotel_add.blade.php
+++ resources/views/admin/hotel_add.blade.php
@@ -1,37 +1,36 @@
 @extends('layout.admin')
 
-@section('title', '宿の管理')
+@section('title', '宿情報の新規登録')
 
 @section('content')
 <div class="search-form">
     <h2>宿情報の新規登録</h2>
-    <form action="" method='post' enctype='multipart/form-data'>
+    <form action="{{ route('hotel.add.confirm') }}" method='post' enctype='multipart/form-data'>
         @csrf
-
         <table>
             <tr>
                 <th>宿名：</th>
-                <td><input type="text" name='hotel_name'></td>
+                <td><input type="text" name='hotel_name' value="{{ old('hotel_name') }}"></td>
             </tr>
             <tr>
                 <th>宿分類：</th>
-                <td><select name="hotel_code">
+                <td><select name="hotel_code" value="{{ old('hotel_code') }}">
                         <option value="0" selected>選択してください</option>
-                        <option value="1">1.シティホテル</option>
-                        <option value="2">2.リゾートホテル</option>
-                        <option value="3">3.ビジネスホテル</option>
-                        <option value="4">4.旅館</option>
-                        <option value="5">5.民宿</option>
-                        <option value="6">6.ペンション</option>
+                        <option value="1.シティホテル">1.シティホテル</option>
+                        <option value="2.リゾートホテル">2.リゾートホテル</option>
+                        <option value="3.ビジネスホテル">3.ビジネスホテル</option>
+                        <option value="4.旅館">4.旅館</option>
+                        <option value="5.民宿">5.民宿</option>
+                        <option value="6.ペンション">6.ペンション</option>
                     </select></td>
             </tr>
             <tr>
                 <th>郵便番号：</th>
-                <td><input type="text" name='hotel_postal'><small>※ハイフンあり</small></td>
+                <td><input type="text" name='hotel_postal' value="{{ old('hotel_postal') }}"><small>※ハイフンあり</small></td>
             </tr>
             <tr>
                 <th rowspan='3'>住所：</th>
-                <td><select name="prefectures">
+                <td><select name="prefectures" value="{{ old('prefectures') }}">
                         <option value="" selected>選択してください</option>
                         <option value="北海道">北海道</option>
                         <option value="青森県">青森県</option>
@@ -84,23 +83,30 @@
                     </select></td>
             </tr>
             <tr>
-                <td><input type="text" name='city' placeholder='市区町村'></td>
+                <td><input type="text" name='city' placeholder='市区町村' value="{{ old('city') }}"></td>
             </tr>
             <tr>
-                <td><input type="text" name='block' placeholder='番地'></td>
+                <td><input type="text" name='block' placeholder='番地' value="{{ old('block') }}"></td>
             </tr>
             <tr>
                 <th>電話番号：</th>
-                <td><input type="text" name='hotel_tel'><small>※ハイフンあり</small></td>
+                <td><input type="text" name='hotel_tel' value="{{ old('hotel_tel') }}"><small>※ハイフンあり</small></td>
+            </tr>
+            <tr>
+                <th>チェックイン時間：</th>
+                <td><input type="time" name='checkin_time' value="{{ old('checkin_time') }}"></td>
+            </tr>
+            <tr>
+                <th>チェックアウト時間：</th>
+                <td><input type="time" name='checkout_time' value="{{ old('checkout_time') }}"></td>
             </tr>
             <tr>
                 <th>宿のイメージ写真：</th>
                 <td><input type="file" name='hotel_image'></td>
             </tr>
         </table>
-        <button><a href="">戻る</a></button>
+        <button><a href="{{ route('hotel.find') }}">戻る</a></button>
         <button type='submit'>確認</button>
     </form>
-
 </div>
 @endsection
