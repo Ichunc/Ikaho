15c9d92c29a8e1f1d9ae61eb906be75bb24d258b
mmrakt <48762864+mmrakt@users.noreply.github.com>
2020-05-22 10:58:53 +0900

    会員の宿検索機能追加
    ・ルーティング追加
    ・HotelControllerにindMemberメソッド追加
    ・HotelModelのgetメソッド修正

--- app/routes/web.php
+++ app/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/member', 'MemberController@index')->name('member.index');
+Route::post('/member', 'HotelController@findMember')->name('member.hotel.find');
+
+Route::get('/member/find_hotel', 'HotelController@findMember')->name('member.hotel.find');
+Route::post('/member/find_hotel', 'HotelController@findMember')->name('member.hotel.find');
