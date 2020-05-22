155524e74fc41cbb0cad6a2aa7264b4ed07e0fcf
smallwest024 <65526704+smallwest024@users.noreply.github.com>
2020-05-22 18:05:55 +0900

    会員登録のれいあうとの若干の変更
    バリデーション用のエラー出力場所を追加

--- resources/views/views/member/member_add_confirm.blade.php
+++ resources/views/views/member/member_add_confirm.blade.php
@@ -3,62 +3,74 @@
 @section('title', '会員登録')
 
 @section('content')
-<div class="search-form">
   <h2>会員登録確認</h2>
-  <form action="" method='post' enctype='multipart/form-data'>
+  <form action="createAccount" method='post'>
     @csrf
-
     <table>
-      <input type='hidden' name='name' value=''>
+      <input type='hidden' name='username' value="{{ $data['username'] }}">
+      <tr>
+        <th>ユーザーネーム：</th>
+        <td>{{ $data['username'] }}</td>
+      </tr>
+      <input type='hidden' name='family_name' value="{{ $data['family_name'] }}">
+      <input type='hidden' name='first_name' value="{{ $data['first_name'] }}">
       <tr>
         <th>お名前：</th>
-        <td>山田　太郎</td>
+        <td>{{ $data['family_name'] }}　{{ $data['first_name'] }}</td>
       </tr>
 
-      <input type="hidden" name='name_kana' value=''>
+      <input type='hidden' name='family_name_kana' value="{{ $data['family_name_kana'] }}">
+      <input type='hidden' name='first_name_kana' value="{{ $data['first_name_kana'] }}">
       <tr>
-        <th>ふりかな：</th>
-        <td>やまだ　たろう</td>
+        <th>かな：</th>
+        <td>{{ $data['family_name_kana'] }}　{{ $data['first_name_kana'] }}</td>
       </tr>
 
-      <input type="hidden" name='sex' value=''>
+      <input type="hidden" name='sex' value="{{ $data['sex'] }}">
       <tr>
         <th>性別：</th>
-        <td>男</td>
+        <td>{{ $data['sex'] }}</td>
       </tr>
 
-      <input type="hidden" name='postal' value=''>
+      <input type="hidden" name='postal' value="{{ $data['postal'] }}">
       <tr>
         <th>郵便番号：</th>
-        <td>〒123-1234</td>
+        <td>〒{{ $data['postal'] }}</td>
       </tr>
 
-      <input type="hidden" name='address' value=''>
+      <input type="hidden" name='prefectures' value="{{ $data['prefectures'] }}">
+      <input type="hidden" name='city' value="{{ $data['city'] }}">
+      <input type="hidden" name='block' value="{{ $data['block'] }}">
       <tr>
         <th>住所：</th>
-        <td>東京都渋谷区○○　○○</td>
+        <td>{{ $data['prefectures'] }}{{ $data['city'] }}{{ $data['block'] }}</td>
       </tr>
 
-      <input type="hidden" name='tel' value=''>
+      <input type="hidden" name='tel' value="{{ $data['tel'] }}">
       <tr>
         <th>電話番号：</th>
-        <td>09012345678</td>
+        <td>{{ $data['tel'] }}</td>
       </tr>
 
-      <input type="hidden" name='email' value=''>
+      <input type="hidden" name='email' value="{{ $data['email'] }}">
       <tr>
         <th>Eメールアドレス：</th>
-        <td>ikaho@gmail</td>
+        <td>{{ $data['email'] }}</td>
       </tr>
 
-      <input type="hidden" name='birthday' value=''>
+      <input type="hidden" name='year' value="{{ $data['year'] }}">
+      <input type="hidden" name='month' value="{{ $data['month'] }}">
+      <input type="hidden" name='day' value="{{ $data['month'] }}">
+
       <tr>
-        <th>生年月日</th>
-        <td>1990年1月1日</td>
+        <th>生年月日：</th>
+        <td>{{ $data['year'] }}年{{ $data['month'] }}月{{ $data['day'] }}日</td>
       </tr>
+      <input type="hidden" name='password' value="{{ $data['password'] }}">
+
     </table>
-    <button><a href="">戻る</a></button>
-    <button type='submit'>登録</button>
+      <button type="button" onclick="history.back()">戻る</button>
+      <button type='submit'>登録</button>
   </form>
 
 </div>
