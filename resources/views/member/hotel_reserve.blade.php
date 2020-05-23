ff8991ea85942a3c2c112368afc7df4effd2afd4
mmrakt <48762864+mmrakt@users.noreply.github.com>
2020-05-23 20:02:20 +0900

    会員の宿予約情報入力画面のレイアウト修正

--- resources/views/member/hotel_reserve.blade.php
+++ resources/views/member/hotel_reserve.blade.php
@@ -3,47 +3,51 @@
 @section('title', '予約情報入力')
 
 @section('content')
-<h2>予約情報入力</h2>
-<table border="1" style="border-collapse: collapse">
-    <caption>選択中の宿泊プラン</caption>
-    <tr>
-        <th>プラン名</th>
-        <th>料金</th>
-    </tr>
-    <tr>
-        <td>{{ $data->plan_name }}</td>
-        <td>{{ $data->price }}</td>
-    </tr>
+<caption>選択中の宿泊プラン</caption>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>プラン名</th>
+            <th>料金(/泊)</th>
+        </tr>
+    </thead>
+    <tbody class="bg-white">
+        <tr>
+            <td>{{ $data->plan_name }}</td>
+            <td>¥{{ number_format($data->price) }}</td>
+        </tr>
+    </tbody>
 </table>
-<form action="{{route('reservation.add.confirm', $data['id'])}}" method="post">
-    @csrf
-    <table>
-        <caption>予約フォーム</caption>
-        <tr>
-            <!--ここに表示されるプルダウンの総数は、DBをもとに決めたい　小西-->
-            <th>予約部屋数：</th>
-            <td><select name="room_amount">
-                    <option value="1" selected>1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select></td>
-        </tr>
-        <tr>
-            <!--ほんとはプルダウンのみ表示したい 小西-->
-            <th>チェックイン日：</th>
-            <td><input type="date" name="checkin_date" max="2100-12-31" min="2020-01-01"></td>
-        </tr>
-        <tr>
-            <th>チェックアウト日：</th>
-            <td><input type="date" name="checkout_date" max="2100-12-31" min="2020-01-01"></td>
-        </tr>
-    </table>
-    <button><a href="{{route('member.index')}}">戻る</a></button>
-    <button type='submit'>確認する</button>
-</form>
 
-</form>
+<div class="card">
+    <div class="card-body m-3">
+        <h5 class="card-title">予約情報の入力</h5>
+        <form class="form" action="{{route('reservation.add.confirm', $data['id'])}}" method='post'>
+            @csrf
+            <div class="form-group row">
+                <div class="col-3">
+                    <label for="room_amount">部屋数</label>
+                    <input class="form-control ml-3" type="number" name="room_amount" min="1" max=''>
+                </div>
 
+            </div>
+            <div class="form-group row">
+                <div class="col">
+                    <label for="checkin_date">チェックイン日</label>
+                    <input class="form-control ml-3" type="date" name="checkin_date" max="2100-12-31" min="2020-01-01">
+                </div>
+                <div class="col">
+                    <label for="checkout_date">チェックアウト日</label>
+                    <input class="form-control ml-3" type="date" name="checkout_date" max="2100-12-31" min="2020-01-01">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <button class="btn"><a class="ml-3" href="{{route('member.hotel.find')}}">戻る</a></button>
+                <button class="btn btn-primary ml-3" type='submit'>確認</button>
+            </div>
+        </form>
+    </div>
+</div>
+<br>
 @endsection
