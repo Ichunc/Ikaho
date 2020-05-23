3525ac6ba0aed67c035cf8234e055935c611add6
mmrakt <48762864+mmrakt@users.noreply.github.com>
2020-05-23 19:48:03 +0900

    管理者の宿削除画面のレイアウト修正

--- resources/views/admin/hotel_delete.blade.php
+++ resources/views/admin/hotel_delete.blade.php
@@ -1,42 +1,52 @@
 @extends('layout.admin')
 
-@section('title', '宿の管理')
+@section('title', '宿情報の削除')
 
 @section('content')
-<div class="search-form">
-    <h2>宿情報の削除確認</h2>
-    <form action="" method='post'>
-        @csrf
-
-        <table>
+<div class="card">
+    <div class="card-body m-3">
+        <h5>宿情報の削除</h5>
+        <table class="table">
             <tr>
                 <th>宿名：</th>
-                <td>伊香保の宿</td>
+                <td>{{ $data['hotel_name']}}</td>
             </tr>
             <tr>
-
                 <th>宿分類：</th>
-                <td>3.旅館</td>
+                <td>{{ $data['hotel_code']}}</td>
             </tr>
             <tr>
                 <th>郵便番号：</th>
-                <td>123-4567</td>
+                <td>{{ $data['hotel_postal']}}</td>
             </tr>
             <tr>
                 <th>住所：</th>
-                <td>群馬県渋川市伊香保町</td>
+                <td>{{ $data['hotel_prefecture'].$data['hotel_city'].$data['hotel_block']}}</td>
             </tr>
             <tr>
                 <th>電話番号：</th>
-                <td>03-0000-0000</td>
+                <td>{{ $data['hotel_tel']}}</td>
             </tr>
+            <tr>
+                <th>チェックイン時間：</th>
+                <td>{{ $data['checkin_time']}}</td>
+            </tr>
+            <tr>
+                <th>チェックアウト時間：</th>
+                <td>{{ $data['checkout_time']}}</td>
+            </tr>
+            <input type="hidden" name='hotel_image' value="">
             <tr>
                 <th>宿のイメージ写真：</th>
                 <td></td>
             </tr>
         </table>
-        <button><a href="">戻る</a></button>
-        <button type='submit'>削除実行</button>
-    </form>
+        <form action="{{ route('hotel.remove', $data['id']) }}" method='post'>
+            @csrf
+            <button class="btn"><a href="{{ route('hotel.find') }}">戻る</a></button>
+            <button class="btn btn-danger" type='submit'>削除</button>
+        </form>
+    </div>
 </div>
+<br>
 @endsection
