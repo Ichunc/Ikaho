34cb78475a9b31bae08589280ef0866f1fa62d2a
mmrakt <48762864+mmrakt@users.noreply.github.com>
2020-05-23 19:21:47 +0900

    レイアウト修正

--- resources/views/admin/hotel_add_confirm.blade.php
+++ resources/views/admin/hotel_add_confirm.blade.php
@@ -3,42 +3,34 @@
 @section('title', '宿情報の塘路国確認')
 
 @section('content')
-<div class="search-form">
-    <h2>宿情報の登録確認</h2>
-    <form action="{{ route('hotel.create') }}" method='post' enctype='multipart/form-data'>
-        @csrf
-        <table>
-            <input type="hidden" name='hotel_name' value="{{ $data['hotel_name']}}">
+<div class="card">
+    <div class="card-body m-3">
+        <h5>宿情報の登録確認</h5>
+        <table class="table">
             <tr>
                 <th>宿名：</th>
                 <td>{{ $data['hotel_name']}}</td>
             </tr>
-            <input type="hidden" name='hotel_code' value="{{ $data['hotel_code']}}">
             <tr>
                 <th>宿分類：</th>
                 <td>{{ $data['hotel_code']}}</td>
             </tr>
-            <input type="hidden" name='hotel_postal' value="{{ $data['hotel_postal']}}">
             <tr>
                 <th>郵便番号：</th>
                 <td>{{ $data['hotel_postal']}}</td>
             </tr>
-            <input type="hidden" name='hotel_address' value="{{ $data['prefectures'].$data['city'].$data['block']}}">
             <tr>
                 <th>住所：</th>
-                <td>{{ $data['prefectures'].$data['city'].$data['block']}}</td>
+                <td>{{ $data['hotel_prefecture'].$data['hotel_city'].$data['hotel_block']}}</td>
             </tr>
-            <input type="hidden" name='hotel_tel' value="{{ $data['hotel_tel']}}">
             <tr>
                 <th>電話番号：</th>
                 <td>{{ $data['hotel_tel']}}</td>
             </tr>
-            <input type="hidden" name='checkin_time' value="{{ $data['checkin_time']}}">
             <tr>
                 <th>チェックイン時間：</th>
                 <td>{{ $data['checkin_time']}}</td>
             </tr>
-            <input type="hidden" name='checkout_time' value="{{ $data['checkout_time']}}">
             <tr>
                 <th>チェックアウト時間：</th>
                 <td>{{ $data['checkout_time']}}</td>
@@ -49,9 +41,21 @@
                 <td></td>
             </tr>
         </table>
-        <button><a href="{{ route('hotel.add') }}">戻る</a></button>
-        <button type='submit'>登録</button>
-    </form>
-
+        <form action="{{ route('hotel.create') }}" method='post' enctype='multipart/form-data'>
+            @csrf
+            <input type="hidden" name='hotel_name' value="{{ $data['hotel_name']}}">
+            <input type="hidden" name='hotel_code' value="{{ $data['hotel_code']}}">
+            <input type="hidden" name='hotel_postal' value="{{ $data['hotel_postal']}}">
+            <input type="hidden" name='hotel_prefecture' value="{{ $data['hotel_prefecture']}}">
+            <input type="hidden" name='hotel_city' value="{{ $data['hotel_city']}}">
+            <input type="hidden" name='hotel_block' value="{{ $data['hotel_block']}}">
+            <input type="hidden" name='hotel_tel' value="{{ $data['hotel_tel']}}">
+            <input type="hidden" name='checkin_time' value="{{ $data['checkin_time']}}">
+            <input type="hidden" name='checkout_time' value="{{ $data['checkout_time']}}">
+            <button class="btn"><a href="{{ route('hotel.add') }}">戻る</a></button>
+            <button class="btn btn-primary" type='submit'>登録</button>
+        </form>
+    </div>
 </div>
+<br>
 @endsection
