687f1a7445e763144496d8e519b5a28ebb3d8c7e
smallwest024 <65526704+smallwest024@users.noreply.github.com>
2020-05-25 14:41:53 +0900

    会員のログインの実装
    会員のみログインの実装ができる

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,59 +3,62 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//use Request;
 use App\User;
-use App\Http\Requests\SignupRequest;
+use App\Admin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class MemberController extends Controller
 {
 
-  public function login(){
-    return view('guest.login');
-  }
 
-  public function postlogin(Request $request){
-    $username = $request->username;
-    $password = $request->password;
-    if (Auth::attempt(['username' => $username,
-                       'password' => $password])){
-          return redirect('member');
-    } else {
-          return view('guest.login');
+    public function guest_index(){
+      return view('guest.guest');
     }
 
-  }
+    public function guest_login(){
+      return view('guest.login');
+    }
 
+    public function login(Request $request){
+      $username = $request->username;
+      $password = $request->password;
+      if (Auth::attempt(['username' => $username,
+                         'password' => $password])){
+            return redirect('member');
+      } else {
+            return view('guest.login');
+      }
 
+    }
 
     public function signup() {
         return view('member.member_add');
     }
 
-    public function confirminfo(SignupRequest $request) {
+    public function confirminfo(Request $request) {
         $data = $request->all();
         return view('member.member_add_confirm', compact('data'));
     }
 
     public function createAccount(Request $request) {
-        $Users = new User;
+        $User = new User;
         $form = $request->all();
         unset($form['_token']);
 
+        //nameを一応作成
+        $User->name = $request->family_name . $request->first_name;
         //住所つなげる
-        $Users->address = $request->prefectures . $request->city . $request->block;
-
-        //nameつける
-        $Users->name = $request->family_name . $request->first_name;
+        $User->address = $request->prefectures . $request->city . $request->block;
         //日付をｰで区切る
-
         $birthday = $request->year . '-' . $request->month . '-' . $request->day;
         //日付のフォーマットを変更する（月と日の前に0をつける）
         $dateformat = new \DateTime($birthday);
-        $Users->birthday = $dateformat->format('Y-m-d');
-        $Users->password = Hash::make($request->password);
-        $Users->fill($form)->save();
+        $User->birthday = $dateformat->format('Y-m-d');
+        //パスワードをハッシュ化する
+        $User->password = Hash::make($request->password);
+        $User->fill($form)->save();
         /*
           $Member->username         = $request->username;
           $Member->family_name      = $request->family_name;
@@ -67,11 +70,56 @@ class MemberController extends Controller
           $Member->tel              = $request->tel;
           $Member->email            = $request->email;
           $Member->birthday         = $request->birthday;
-          $Member->password         = Hash::make($request->password);
+          $Member->password         = $request->password;
         */
-
         //$birthday = $_POST["year"] . '-' .  $_POST["month"] . '-' . $_POST["day"];
-        return redirect('/member/index');
+        return redirect('/guest/login');
     }
+    public function member_index(){
+      return view('member.index');
+    }
+
+
+
+
+
+
+/*
+    //admin
+    public function admin_login(Request $request){
+      $admin_name = $request->admin_name;
+      $password = $request->password;
+      if (Auth::attempt(['admin_name' => $admin_name,
+                         'password' => $password])){
+            return redirect('admin');
+      } else {
+            return view('admin.login');
+      }
+    }
+
+    public function admin_login_view(){
+      return view('admin.login');
+    }
+
+    public function admin_signup(){
+      return view('admin.admin_add');
+    }
+    public function admin_confirmInfo(Request $request) {
+        $data = $request->all();
+        return view('admin.admin_add_confirm', compact('data'));
+    }
+
+    public function admin_createAccount(Request $request) {
+        $Admin = new Admin;
+        $form = $request->all();
+        unset($form['_token']);
+        $Admin->password = Hash::make($request->password);
+        $Admin->fill($form)->save();
+        return redirect('/admin/login');
+    }
+*/
+
+
+
 
 }
