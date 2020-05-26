cc89dd8509084c5d9196a6574c85ac5860af0330
mmrakt <48762864+mmrakt@users.noreply.github.com>
2020-05-26 11:52:21 +0900

    修正

--- resources/views/admin/plan/plan_add_confirm.blade.php
+++ resources/views/admin/plan/plan_add_confirm.blade.php
@@ -1,42 +1,66 @@
 @extends('layout.admin')
 
-@section('title', '宿泊プラン情報の登録確認')
+@section('title', '宿情報の塘路国確認')
 
 @section('content')
 <div class="card">
     <div class="card-body m-3">
-        <h5>宿泊プラン情報の登録確認</h5>
+        <h5>宿情報の登録確認</h5>
         <table class="table">
             <tr>
-                <th>プラン名：</th>
-                <td>{{ $data['plan_name']}}</td>
+                <th>宿名：</th>
+                <td>{{ $data['hotel_name']}}</td>
             </tr>
             <tr>
-                <th>プランの特徴：</th>
-                <td>{{ $data['description']}}</td>
+                <th>宿分類：</th>
+                <td>{{ $data['hotel_code']}}</td>
             </tr>
             <tr>
-                <th>料金(/泊)：</th>
-                <td>\{{ number_format($data['price'])}}</td>
+                <th>郵便番号：</th>
+                <td>{{ $data['hotel_postal']}}</td>
             </tr>
             <tr>
-                <th>部屋数：</th>
-                <td>{{ $data['room_amount']}}</td>
+                <th>住所：</th>
+                <td>{{ $data['hotel_prefecture'].$data['hotel_city'].$data['hotel_block']}}</td>
             </tr>
             <tr>
-                <th>備考：</th>
-                <td>{{ $data['note']}}</td>
+                <th>電話番号：</th>
+                <td>{{ $data['hotel_tel']}}</td>
+            </tr>
+            <tr>
+                <th>チェックイン時間：</th>
+                <td>{{ $data['checkin_time']}}</td>
+            </tr>
+            <tr>
+                <th>チェックアウト時間：</th>
+                <td>{{ $data['checkout_time']}}</td>
+            </tr>
+            <input type="hidden" name='hotel_image' value="">
+            <tr>
+                <th>宿のイメージ写真：</th>
+                <td>@if(isset($file_name))
+                    <img src="{{ asset('storage/img/' . $file_name) }}">
+                    @else
+                    写真なし
+                    @endif
+                </td>
             </tr>
         </table>
-        <form action="{{ route('plan.add.complete') }}" method='post' enctype='multipart/form-data'>
+        <form action="{{ route('hotel.create') }}" method='post' enctype='multipart/form-data'>
             @csrf
-            <input type="hidden" name='plan_name' value="{{ $data['plan_name']}}">
-            <input type="hidden" name='hotel_id' value="{{ $data['hotel_id']}}">
-            <input type="hidden" name='description' value="{{ $data['description']}}">
-            <input type="hidden" name='price' value="{{ $data['price']}}">
-            <input type="hidden" name='room_amount' value="{{ $data['room_amount']}}">
-            <input type="hidden" name='note' value="{{ $data['note']}}">
-            <button class="btn"><a href="{{ route('plan.add') }}">戻る</a></button>
+            <input type="hidden" name='hotel_name' value="{{ $data['hotel_name']}}">
+            <input type="hidden" name='hotel_code' value="{{ $data['hotel_code']}}">
+            <input type="hidden" name='hotel_postal' value="{{ $data['hotel_postal']}}">
+            <input type="hidden" name='hotel_prefecture' value="{{ $data['hotel_prefecture']}}">
+            <input type="hidden" name='hotel_city' value="{{ $data['hotel_city']}}">
+            <input type="hidden" name='hotel_block' value="{{ $data['hotel_block']}}">
+            <input type="hidden" name='hotel_tel' value="{{ $data['hotel_tel']}}">
+            @if(isset($file_name))
+            <input type="hidden" name='hotel_image' value="{{ $file_name}}">
+            @endif
+            <input type="hidden" name='checkin_time' value="{{ $data['checkin_time']}}">
+            <input type="hidden" name='checkout_time' value="{{ $data['checkout_time']}}">
+            <button class="btn"><a href="{{ route('hotel.add') }}">戻る</a></button>
             <button class="btn btn-primary" type='submit'>登録</button>
         </form>
     </div>
